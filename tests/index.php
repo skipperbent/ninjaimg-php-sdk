72e39821f6d2441adc9fd7109c146313bbf43bec
Simon Sessingø <simon.sessingoe@gmail.com>
2015-12-19 18:06:10 +0100

    [TASK] Optimisations

--- tests/index.php
+++ tests/index.php
@@ -2,13 +2,13 @@
 
 require_once '../vendor/autoload.php';
 
-$url = new \NinjaImg\ServiceImage('http://local.service.ninjaimg/1914109_142926756975_2647065_n.jpg');
+/*$url = new \NinjaImg\ServiceImage('http://local.service.ninjaimg/1914109_142926756975_2647065_n.jpg');
 
-die($url->height(200)->width(200)->getUrl());
+die($url->height(200)->width(200)->getUrl());*/
 
 
-$image = new \NinjaImg\ServiceUpload('local.service.ninjaimg', 'B51099F3BE3747569B0EAD286DB5CF6C');
-$content = file_get_contents('11705146_10155774660785543_5213892747026074050_n.jpg');
+$image = new \NinjaImg\ServiceUpload('local.service2.ninjaimg', 'B51099F3BE3747569B0EAD286DB5CF6C');
+$content = file_get_contents('funny.jpg');
 $response = $image->upload($content, '/test/dement-mand.jpg');
 
 die(var_dump($image->delete('/test/dement-mand.jpg')));
